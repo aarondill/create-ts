35d9516d68f493101279d82746cf05db6d968fa2
aarondill <aaronsacks2006@gmail.com>
2023-02-06 12:28:20 -0600

    fix eslint root config

--- src/after/eslint.ts
+++ src/after/eslint.ts
@@ -13,12 +13,13 @@ async function setEslintRootFalse({
 	const files = await globby(".eslintrc.*", { cwd: packageDir, dot: true });
 	for (const file of files) {
 		const eslintrcString = await fs.readFile(file, "utf-8");
-		const rootTrueRegex = /^(\s*)("?)root(\2):\s*true(,?\s*)$/;
+		const rootTrueRegex = /^(\s*)("?)root(\2):\s*true(,?\s*)$/m;
 		// Only replace first one, is prob the only one
 		const rootFalseString = eslintrcString.replace(
 			rootTrueRegex,
-			"$1root: false$3"
+			"$1$2root$3: false$4"
 		);
+		console.log(rootFalseString.includes("root: false"));
 		await fs.writeFile(file, rootFalseString, "utf-8");
 	}
 }
